b336011863f8fd79e13f589c7a6297d8a6e76126
Ira <nikulochkina.irina@yandex.ru>
2022-08-15 18:38:40 +0300

    added classnames libraru

--- src/index.js
+++ src/index.js
@@ -3,31 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const postsData = [
-  { message: 'Hi', id: 1, likesCount: 20 },
-  { message: 'bye', id: 2, likesCount: 0 },
-  { message: 'bye', id: 2, likesCount: 88 },
-];
+import state from './components/redux/state';
 
-const dialogueInfo = [
-  { userName: 'Color', id: '1' },
-  { userName: 'Floor', id: '2' },
-  { userName: 'Furniture', id: '3' },
-  { userName: 'Workers', id: '4' },
-  { userName: 'Light', id: '5' },
-];
-
-const messageContent = [
-  { messageText: 'Your workers are ready to start' },
-  { messageText: 'Your order is delayed' },
-  { messageText: 'Your color has been changed' },
-];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App
-    postsData={postsData}
-    dialogueInfo={dialogueInfo}
-    messageContent={messageContent}
+    appState = {state}
   />
 );
 
