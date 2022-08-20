5f5f6652e6d5ae4100a2aa389eb72db4f0f620ea
Ira <nikulochkina.irina@yandex.ru>
2022-08-20 17:50:48 +0300

    так я чет зарефакторила и теперь посты не добавляются ааааааа *орет что все опять сломала*

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,49 +1,54 @@
-let rerenderEntireTree = () => {
-    console.log('State changed.');
-};
-let state = {
-    updates: {
-        postsData: [
-            {message: 'Hi', id: 1, likesCount: 20},
-            {message: 'bye', id: 2, likesCount: 0},
-            {message: 'bye', id: 2, likesCount: 88},
-        ],
-        newPostText: 'blah-blah'
+let store = {
+    _state: {
+        updates: {
+            postsData: [
+                {message: 'Hi', id: 1, likesCount: 20},
+                {message: 'bye', id: 2, likesCount: 0},
+                {message: 'bye', id: 2, likesCount: 88},
+            ],
+            newPostText: 'blah-blah'
+        },
+
+        chat: {
+            dialogueInfo: [
+                {userName: 'Color', id: '1'},
+                {userName: 'Floor', id: '2'},
+                {userName: 'Furniture', id: '3'},
+                {userName: 'Workers', id: '4'},
+                {userName: 'Light', id: '5'},
+            ],
+            messageContent: [
+                {messageText: 'Your workers are ready to start'},
+                {messageText: 'Your order is delayed'},
+                {messageText: 'Your color has been changed'},
+            ],
+        },
+    },
+    _callSubscriber() {
+        console.log('State changed.');
+    },
+    getState() {
+        return this._state;
+    },
+    addPost() {
+        let newPost = {
+            message: this.state.updates.newPostText,
+            id: 5,
+            likesCount: 0,
+        };
+        this.state.updates.postsData.push(newPost);
+        this.state.updates.newPostText = '';
+        this._callSubscriber(this.state);
+    },
+    updateNewPostText(newText) {
+        this.state.updates.newPostText = newText;
+        this._callSubscriber(this.state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     },
 
-    chat: {
-        dialogueInfo: [
-            {userName: 'Color', id: '1'},
-            {userName: 'Floor', id: '2'},
-            {userName: 'Furniture', id: '3'},
-            {userName: 'Workers', id: '4'},
-            {userName: 'Light', id: '5'},
-        ],
-        messageContent: [
-            {messageText: 'Your workers are ready to start'},
-            {messageText: 'Your order is delayed'},
-            {messageText: 'Your color has been changed'},
-        ],
-    },
 };
 
-export const addPost = () => {
-    let newPost = {
-        message: state.updates.newPostText,
-        id: 5,
-        likesCount: 0,
-    };
-    state.updates.postsData.push(newPost);
-    state.updates.newPostText = '';
-    rerenderEntireTree(state);
-};
-export const updateNewPostText = (newText) => {
-state.updates.newPostText = newText;
-    rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {addPost, subscribe, updateNewPostText} from './components/redux/state';
+import store from './components/redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
     root.render(
         <App
             state={state}
-            addPost={addPost}
-            updateNewPostText={updateNewPostText}
+            addPost={store.addPost.bind(store)}
+            updateNewPostText={store.updateNewPostText.bind(store)}
         />
     );
 };
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
 reportWebVitals();
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
