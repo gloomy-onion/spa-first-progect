e839e54312f76bc16d36ba222a8ddf0aa23c2d40
Ira <nikulochkina.irina@yandex.ru>
2022-10-16 14:14:03 +0300

    я сделала не так как ты сказал но оно работает

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Header from './components/Header/Header';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import routesElements from './components/common/routes/routesElements';
 
+const renderPath = () => routesElements.map(({path, exact, element}) =>
+    (<Route exact={exact} path={path} element={element}/>));
+
 const App = () => {
-    const routeComponents = routesElements.map(({path, exact, element}) =>
-        (<Route exact={exact} path={path} element={element}/>));
     return (<BrowserRouter>
         <div className="App">
             <Header/>
             <Routes>
-                {routeComponents}
+                {renderPath()}
             </Routes>
             <Footer/>
         </div>
