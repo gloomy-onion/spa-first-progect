01420159f2322c26f2341fa6ecc1e0fe105caf4a
Ira <nikulochkina.irina@yandex.ru>
2022-11-08 13:49:25 +0300

    created a class component

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -3,66 +3,66 @@ import styles from "./team.module.css";
 import axios from "axios";
 import userImage from "../../assets/img/userImage.png";
 
-const Team = (props) => {
-  const getUsers = () => {
-    if (props.users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          props.setUsers(response.data.items);
-        });
-    }
-  };
-  return (
-    <div>
-      <button onClick={getUsers}>Get Users</button>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img
-                src={u.photos.small != null ? u.photos.small : userImage}
-                className={styles.userPhoto}
-                alt={""}
-              />
-            </div>
-            <div>
-              {" "}
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  {" "}
-                  Unfollow{" "}
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  {" "}
-                  Follow{" "}
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
-            </span>
-            <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
-            </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+class Team extends React.Component {
 
+  componentDidMount() {
+      axios
+          .get("https://social-network.samuraijs.com/api/1.0/users")
+          .then((response) => {
+              this.props.setUsers(response.data.items);
+          });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id}>
+            <span>
+              <div>
+                <img
+                  src={u.photos.small != null ? u.photos.small : userImage}
+                  className={styles.userPhoto}
+                  alt={""}
+                />
+              </div>
+              <div>
+                {" "}
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    {" "}
+                    Unfollow{" "}
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    {" "}
+                    Follow{" "}
+                  </button>
+                )}
+              </div>
+            </span>
+            <span>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
+              </span>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 export default Team;
