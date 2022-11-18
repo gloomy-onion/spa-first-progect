a59ebcd3d67e90b293fa0b0917101089256f4604
Ira <nikulochkina.irina@yandex.ru>
2022-11-18 18:09:47 +0300

    то что мы обсуждали с контактами у меня что то получилось !!!! я сама прикинь, не искала готовое решение только проблема, что я не поняла как их с новой строки делать каждый

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,11 +5,18 @@ const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />;
   }
+  const ContactWebsites = Object.entries(props.profile.contacts).map(
+    ([key, value]) => `${key[0].toUpperCase() + key.slice(1)}: ${value} `
+  );
+
   return (
     <div>
       <img src={props.profile.photos.large} alt={""} />
       <div>{props.profile.aboutMe}</div>
-        <div>Facebook: {props.profile.contacts.facebook}</div>
+      <div>Contacts:</div>
+      <div>
+        <span>{ContactWebsites}</span>
+      </div>
     </div>
   );
 };
