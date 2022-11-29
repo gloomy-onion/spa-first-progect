4a47f25acddfab42b6a2a945d709b6a7791306e4
Ira <nikulochkina.irina@yandex.ru>
2022-11-29 20:29:40 +0300

    added renderContacts

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,28 +1,18 @@
 import React from "react";
 import Preloader from "../../common/Preloader/Preloader";
+import {renderContacts} from '../../../helpers/profile.jsx';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />;
   }
-  const RenderContacts = (profile) => {
-    const contactWebsites = Object.entries(props.profile.contacts).map(
-      ([name, link]) => (
-        <div>
-          {name[0].toUpperCase() + name.slice(1)}:
-          {link != null ? link : "not defined"}
-        </div>
-      )
-    );
-    return <div> {contactWebsites} </div>;
-  };
 
   return (
     <div>
       <img src={props.profile.photos.large} alt={""} />
       <div>{props.profile.aboutMe}</div>
       <div>Contacts:</div>
-      <RenderContacts />
+      {renderContacts(props.profile)}
     </div>
   );
 };
