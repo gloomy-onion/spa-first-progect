eec56315bded849f761569c50803e250fa9e4b96
Ira <nikulochkina.irina@yandex.ru>
2023-03-14 13:27:00 +0300

    пока имею доступ закоммичу. добавила валидацию к формам, деструктурировала в формконтрол

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import styles from './FormsControls.module.css';
+import React from "react";
+import styles from "./FormsControls.module.css";
 
-export const Textarea = ({input, meta, ...props}) => {
+const FormControl = ({ input, meta, child, ...props }) => {
+  const hasError = meta.touched && meta.error;
 
-    const hasError = meta.touched && meta.error;
-
-    return (
-      <div className={styles.formControl + "" + (hasError ? styles.error : "")}>
-        <div>
-          <textarea {...input} {...props} />
-        </div>
-        {hasError && <span>{meta.error}</span>}
-      </div>
-    );
+  return (
+    <div className={styles.formControl + "" + (hasError ? styles.error : "")}>
+      <div>{props.children}</div>
+      {hasError && <span>{meta.error}</span>}
+    </div>
+  );
 };
 
-export const Input = ({input, meta, ...props}) => {
+export const Textarea = (props) => {
+  const { input, meta, child, ...restProps } = props;
+  return (
+    <FormControl {...props}>
+      <textarea {...input} {...restProps} />
+    </FormControl>
+  );
+};
 
-    const hasError = meta.touched && meta.error;
-
-    return (
-      <div className={styles.formControl + "" + (hasError ? styles.error : "")}>
-        <div>
-          <input {...input} {...props} />
-        </div>
-        {hasError && <span>{meta.error}</span>}
-      </div>
-    );
+export const Input = (props) => {
+  const { input, meta, child, ...restProps } = props;
+  return (
+    <FormControl {...props}>
+      <input {...input} {...restProps} />
+    </FormControl>
+  );
 };
