85e3d729cd4478076d3de950e2c99880b08b1740
Ira <nikulochkina.irina@yandex.ru>
2023-05-02 17:54:24 +0300

    добавили расширение в браузер и тесты которые работают

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-
-
 class ProfileStatus extends React.Component {
   state = {
     editMode: false,
---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.test.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import ProfileStatus from "./ProfileStatus";
+
+describe("ProfileStatus component", () => {
+  test("status from props should be in the state", () => {
+    const component = create(<ProfileStatus status="blabla" />);
+    const instance = component.getInstance();
+    expect(instance.state.status).toBe("blabla");
+  });
+  test("after creation input shouldn't be displayed", () => {
+    const component = create(<ProfileStatus status="blabla" />);
+    const root = component.root;
+    expect(() => {
+      const input = root.findByType("input");
+    }).toThrow();
+  });
+  test("after creation span with status should be displayed", () => {
+    const component = create(<ProfileStatus status="blabla" />);
+    const root = component.root;
+    const span = root.findByType("span");
+    expect(span).not.toBeNull();
+  });
+  test("after creation span should have text", () => {
+    const component = create(<ProfileStatus status="blabla" />);
+    const root = component.root;
+    const span = root.findByType("span");
+    expect(span.children[0]).toBe("blabla");
+  });
+  test("editmode input", () => {
+    const component = create(<ProfileStatus status="blabla" />);
+    const root = component.root;
+    const span = root.findByType("span");
+    span.props.onDoubleClick();
+    const input = root.findByType("input");
+    expect(input.props.value).toBe("blabla");
+  });
+});
