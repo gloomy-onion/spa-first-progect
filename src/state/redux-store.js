5e8cb3f4378a9d37770b919ef5632ef65d0ce735
Ira <nikulochkina.irina@yandex.ru>
2023-05-02 12:29:10 +0300

    добавили расширение в браузер

--- src/state/redux-store.js
+++ src/state/redux-store.js
@@ -1,12 +1,16 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from 'redux';
+import {
+  applyMiddleware,
+  combineReducers, compose,
+  legacy_createStore as createStore,
+} from "redux";
 import updatesReducer from "./updates-reducer";
 import chatReducer from "./chat-reducer";
 import teamReducer from "./team-reducer";
-import authReducer from './auth-reducer';
+import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
-import viewerReducer from './viewer-reducer';
-import {reducer as formReducer} from 'redux-form';
-import appReducer from './app-reducer';
+import viewerReducer from "./viewer-reducer";
+import { reducer as formReducer} from "redux-form";
+import appReducer from "./app-reducer";
 
 const reducers = combineReducers({
   updates: updatesReducer,
@@ -15,11 +19,16 @@ const reducers = combineReducers({
   auth: authReducer,
   viewer: viewerReducer,
   form: formReducer,
-  app: appReducer
+  app: appReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
-window.store = store;
+
+window.__store__ = store;
 
 export default store;
